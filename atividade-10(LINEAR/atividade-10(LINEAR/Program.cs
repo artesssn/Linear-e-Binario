17b1c97d4c0c9b86d3a76139cde3404307f92b80
Joao Gabriel <123424925+artesssn@users.noreply.github.com>
2025-04-08 19:46:15 -0300

    Add files via upload

---
+++ atividade-10(LINEAR/atividade-10(LINEAR/Program.cs
@@ -0,0 +1,30 @@
+﻿    static int BuscarPalavra(string texto, string palavra)
+    {
+    
+        return texto.IndexOf(palavra);
+    }
+
+    static void Main(string[] args)
+    {
+   
+        Console.WriteLine("Digite o texto:");
+        string texto = Console.ReadLine();
+
+       
+        Console.WriteLine("Digite a palavra que deseja buscar:");
+        string palavra = Console.ReadLine();
+
+        
+        int posicao = BuscarPalavra(texto, palavra);
+
+       
+        if (posicao != -1)
+        {
+            Console.WriteLine($"A palavra '{palavra}' foi encontrada no índice {posicao}.");
+        }
+        else
+        {
+            Console.WriteLine($"A palavra '{palavra}' não foi encontrada no texto.");
+        }
+    }
+
